d17a791e106dc7c8d9d8637f74b1646ede9e7d3c
darkground <blockads32@gmail.com>
2025-05-13 22:56:04 +0300

    replace modelviewset with mixins to allow only certain requests

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -4,18 +4,25 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import pagination
+from rest_framework import permissions
+from rest_framework import mixins
 
 from core.models import User
 from .serializers import UserSerializer
 
 # Create your views here.
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(
+        mixins.CreateModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     pagination_class = pagination.LimitOffsetPagination
+    permission_classes = [permissions.AllowAny]
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
     def me(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
