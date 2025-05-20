519bac7a9b00afbc5af58cd9faa0c2f3cda9adf5
darkground <blockads32@gmail.com>
2025-05-20 15:02:07 +0300

    change authenticated user checking

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -2,14 +2,18 @@ from rest_framework import serializers
 
 from core.models import Subscription, User
 
+
 class UserSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField('get_is_subscribed')
 
     def get_is_subscribed(self, obj):
-        user = getattr(self.context.get('request'), 'user', None)
-        if user and not user.is_anonymous:
-            return Subscription.objects.filter(
-                user__exact=user, subscribed_to__exact=obj).exists()
+        user = getattr(self.context.get('request'), 'user')
+        if user and user.is_authenticated:
+            return (
+                user.is_authenticated
+                and
+                Subscription.objects.filter(user__exact=user, subscribed_to__exact=obj).exists()
+            )
         return False
 
     class Meta:
