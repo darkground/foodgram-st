755544715176ec2ec1653a84e5816b743802574b
darkground <blockads32@gmail.com>
2025-05-20 15:43:32 +0300

    add new models to admin

--- backend/foodgram/core/admin.py
+++ backend/foodgram/core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from core.models import Subscription, User
+from .models import (
+    Ingredient,
+    IngredientInRecipe,
+    Recipe,
+    Subscription,
+    User
+)
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Subscription)
+admin.site.register(Ingredient)
+admin.site.register(IngredientInRecipe)
+admin.site.register(Recipe)
