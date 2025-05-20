c99da628688abdaedb393599f20f7ce0acb37ac6
darkground <blockads32@gmail.com>
2025-05-20 18:33:47 +0300

    register new models in admin

--- backend/foodgram/core/admin.py
+++ backend/foodgram/core/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import (
+    Favorite,
     Ingredient,
     IngredientInRecipe,
     Recipe,
+    ShoppingCart,
     Subscription,
     User
 )
@@ -15,3 +17,5 @@ admin.site.register(Subscription)
 admin.site.register(Ingredient)
 admin.site.register(IngredientInRecipe)
 admin.site.register(Recipe)
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
