86824cbdf748564d74b0e91fcdca2bfab2607106
darkground <blockads32@gmail.com>
2025-05-22 20:37:28 +0300

    run_dev now doesnt clear data, only imports it

--- infra/run_development.ps1
+++ infra/run_development.ps1
@@ -1,41 +1,24 @@
 param(
     [switch]$Setup = $false,
-    [switch]$ClearData = $false,
     [string]$EnvFile = ".env_dev"
 )
 
 Write-Host "Deploying Foodgram for local development"
 $Infra = $PSScriptRoot
 $Backend = Resolve-Path "$Infra/../backend"
-$Initdata = Resolve-Path "$Backend/data/ingredients.json"
+$Initdata = Resolve-Path "$Backend/data/initial_data.json"
 $ManagePy = Resolve-Path "$Backend/foodgram/manage.py"
+$MediaVolume = Resolve-Path "$Infra/../data/volume"
+$MediaBackend = "$Backend/foodgram/media"
 $Venv = Resolve-Path "$Backend/venv/Scripts/Activate.ps1"
 $Requirements = Resolve-Path "$Backend/requirements.txt"
+$Env = Resolve-Path "$PSScriptRoot/$EnvFile"
 
 Write-Host "** Activating virtual environment **"
 . $Venv | Out-Null
 
-if ($Setup) {
-Write-Host "** Installing virtual environment packages **"
-pip install -r $Requirements | Out-Null
-}
-
-if ($ClearData) {
-Write-Host "** Clearing previous database data **"
-
-"from core.models import Ingredient; Ingredient.objects.all().delete()" | py $ManagePy shell | Out-Null
-
-"from django.contrib.auth import get_user_model; User = get_user_model(); `
-usernames_list = ['root', 'vasya.ivanov', 'second-user', 'third-user-username', 'NoEmail', 'NoFirstName', 'NoLastName', 'NoPassword', 'TooLongEmail', `
-'the-username-that-is-150-characters-long-and-should-not-pass-validation-if-the-serializer-is-configured-correctly-otherwise-the-current-test-will-fail-', `
-'TooLongFirstName', 'TooLongLastName', 'InvalidU`$ername', 'EmailInUse']; `
-delete_num, _ = User.objects.filter(username__in=usernames_list).delete(); `
-exit(1) if not delete_num else exit(0);" | py $ManagePy shell | Out-Null
-
-}
-
 Write-Host "** Setting up environment variables **"
-Get-Content $EnvFile | ForEach-Object {
+Get-Content $Env | ForEach-Object {
     $name, $value = $_.split('=')
 
     if ([string]::IsNullOrWhiteSpace($name) -or $name.Contains('#')) { return }
@@ -47,16 +30,17 @@ Set-Content env:DJANGO_IS_DEBUG True
 Set-Content env:DJANGO_IS_SQLITE3 True
 
 if ($Setup) {
+Write-Host "** Installing virtual environment packages **"
+pip install -r $Requirements | Out-Null
+
 Write-Host "** Setting up migrations **"
 py $ManagePy migrate | Out-Null
 
-Write-Host "** Creating superuser root@foodgram.com:root **"
-"from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'root@foodgram.com', 'root')" | py $ManagePy shell | Out-Null
+Write-Host "** Copying volume data **"
+Copy-Item $MediaVolume $MediaBackend -Recurse
 
-Write-Host "** Importing ingredients data **"
-"from core.models import Ingredient; from json import loads; `
-fl = open(r'$Initdata', mode='r', encoding='utf-8'); jsn = loads(fl.read()); fl.close(); `
-Ingredient.objects.bulk_create([Ingredient(name=item['name'], measurement_unit=item['measurement_unit']) for item in jsn]);" | py $ManagePy shell | Out-Null
+Write-Host "** Importing fixture data **"
+py $ManagePy loaddata $Initdata
 
 Write-Host "** Collecting static **"
 py $ManagePy collectstatic --noinput | Out-Null
