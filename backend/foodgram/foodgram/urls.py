867d7b86fa819c8e48e75e3c3243b44dc79d82b4
darkground <blockads32@gmail.com>
2025-05-22 20:36:50 +0300

    debug static and media

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 from api.views import ShortRedirectView
+from foodgram import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('s/<str:short_link>', ShortRedirectView.as_view(), name='short-link')
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
